ff2f94ae4054e7ace079aa64a838288f60ededb0
David Kirkby <dkirkby@uci.edu>
2018-04-09 09:56:40 -0700

    Fix tabulated PSF calculations

--- coadd/spectra.py
+++ coadd/spectra.py
@@ -7,7 +7,7 @@ import scipy.special
 
 class CoAdder(object):
 
-    def __init__(self, wlen_lo, wlen_hi, max_dispersion, n_grid):
+    def __init__(self, wlen_lo, wlen_hi, wlen_step, max_dispersion):
         """Initialize coadder for 1D binned data.
 
         Parameters
@@ -18,28 +18,28 @@ class CoAdder(object):
         wlen_hi : float
             High edge to use for the internal grid. Should be above the lowest
             value expected in data. Must be > wlen_lo. Units are arbitrary.
+        wlen_step : float
+            Internal step size will rounded down from this value to uniformly
+            cover [wlen_lo, wlen_hi].  Should be ~5x the smallest expected
+            RMS dispersion.
         max_dispersion : float
             The maximum expected dispersion in wlen units. Any PSF with
             tails beyond this value will be truncated.
-        n_grid : int
-            Number of internal grid points to use. Should be ~5x the smallest
-            expected RMS dispersion. Must be > 0. Internal storage requirements
-            scale with n_grid ** 2.
         """
-        if n_grid <= 0:
-            raise ValueError('Expected n_grid > 0.')
-        self.n_grid = n_grid
         if wlen_lo >= wlen_hi:
             raise ValueError('Expected wlen_lo < wlen_hi.')
-        self.grid = np.linspace(wlen_lo, wlen_hi, n_grid)
-        self.grid_scale = (wlen_hi - wlen_lo) / (n_grid - 1.)
+        if wlen_step <= 0:
+            raise ValueError('Expected wlen_step > 0.')
+        self.n_grid = int(np.ceil((wlen_hi - wlen_lo) / wlen_step)) + 1
+        self.grid, self.grid_scale = np.linspace(
+            wlen_lo, wlen_hi, self.n_grid, retstep=True)
         if max_dispersion <= 0:
             raise ValueError('Expected max_dispersion > 0.')
         n = int(np.ceil(max_dispersion / self.grid_scale))
         self.n_psf = 2 * n + 1
         self.psf_grid = np.arange(-n, n+1) * self.grid_scale
-        self.phi_sum = np.zeros(num_grid)
-        self.A_sum = np.zeros((num_grid, num_grid))
+        self.phi_sum = np.zeros(self.n_grid)
+        self.A_sum = np.zeros((self.n_grid, self.n_grid))
         self.root2pi = np.sqrt(2 * np.pi)
 
     def add(self, data, edges, ivar, psf, convolve_with_pixel=True):
@@ -51,8 +51,10 @@ class CoAdder(object):
             Array of N pixel values for this observation.
         edges : array
             Array of N+1 increasing pixels edges for this observation.
+            The first and last edges must be inset enough for the maximum
+            dispersion.
         ivar : array
-            Array of N inverse variance values for this observation. Must
+            Array of N inverse variances for this observation's data. Must
             all be >= 0. Covariances between pixels are assumed to be zero.
         psf : float or array
             The dispersion at each pixel center can be specified four different
@@ -89,39 +91,94 @@ class CoAdder(object):
             raise ValueError('All ivar values must >= 0.')
         if not np.all(np.diff(edges) > 0):
             raise ValueError('Pixel edges are not in increasing order.')
+        if edges[0] + self.psf_grid[0] < self.grid[0]:
+            raise ValueError('First edge not inset enough for dispersion.')
+        if edges[-1] + self.psf_grid[-1] > self.grid[-1]:
+            raise ValueError('Last edge not inset enough for dispersion.')
 
+        # Tabulate the support of each pixel i as gp[i].
         psf = np.atleast_1d(psf)
+        gp = np.zeros((npixels, self.n_grid))
         if len(psf.shape) == 1:
+            # psf specifies either a single RMS, per-pixel RMS, or a
+            # tabulated psf shared by all pixels.
             if len(psf) not in (1, npixels, self.n_psf):
                 raise ValueError('Invalid psf array length.')
             if len(psf.shape) == npixels and npixels == self.n_psf:
                 raise ValueError('Ambiguous psf since npixels = n_psf.')
             if len(psf) == self.n_psf:
-                if not np.allclose(psf.sum(), 1):
-                    raise ValueError('Input psf is not normalized.')
-                psf = psf.reshape(1, self.n_psf)
+                if convolve_with_pixel:
+                    if not np.allclose(psf.sum(), 1):
+                        raise ValueError('Input psf is not normalized.')
+                    for i in range(npixels):
+                        self.psf_convolve(edges[i], edges[i + 1], psf, gp[i])
+                else:
+                    for i in range(npixels):
+                        self.psf_center(edges[i], edges[i + 1], psf, gp[i])
             else:
+                # psf specifies either a single RMS or per-pixel RMS values.
                 if np.any(psf <= 0):
                     raise ValueError('Expected all PSF RMS > 0.')
-                # Tabulate normalized PSF with specified RMS value(s).
-                rms = psf.reshape(1, -1)
-                psf = self.grid_scale * np.exp(
-                    -0.5 * (self.psf_grid / rms) ** 2) / (self.root2pi * rms)
+                # Tabulate PSF with specified RMS value(s).
+                rms = psf.reshape(-1, 1)
+                wlen = self.grid
+                if convolve_with_pixel:
+                    erf_lo = scipy.special.erf(
+                        (edges[:-1, np.newaxis] - wlen) / (np.sqrt(2) * rms))
+                    erf_hi = scipy.special.erf(
+                        (edges[1:, np.newaxis] - wlen) / (np.sqrt(2) * rms))
+                    gp[:] = 0.5 * (erf_hi - erf_lo)
+                else:
+                    wlen0 = 0.5 * (edges[1:] + edges[:-1]).reshape(-1, 1)
+                    dwlen = (edges[1:] - edges[:-1]).reshape(-1, 1)
+                    gp[:] = dwlen * np.exp(
+                        -0.5 * ((wlen - wlen0) / rms) ** 2) / (self.root2pi * rms)
         elif psf.shape == (npixels, self.n_psf):
-            if not np.allclose(psf.sum(axis=1), 1):
-                raise ValueError('Input psfs are not normalized.')
+            if convolve_with_pixel:
+                if not np.allclose(psf.sum(axis=1), 1):
+                    raise ValueError('Input psfs are not normalized.')
+                for i in range(npixels):
+                    self.psf_convolve(edges[i], edges[i + 1], psf[i], gp[i])
+            else:
+                for i in range(npixels):
+                    self.psf_center(edges[i], edges[i + 1], psf[i], gp[i])
         else:
             raise ValueError('Unexpected psf shape.')
-        '''
+
+        return gp
+
+        # Calculate this observations contributions to phi, A.
         phi = np.zeros_like(self.phi_sum)
         A = np.zeros_like(self.A_sum)
-        gp = np.empty((npixels, len(self.x_grid)))
         M = np.digitize(self.x_grid[:, np.newaxis] + self.x_grid, edges)
         for p in range(npixels):
             gp[p] = ((M == p + 1) * psf).sum(axis=1)
-            phi += gp[p] * image[p] * ivar[p]
+            phi += gp[p] * data[p] * ivar[p]
             A += np.outer(gp[p], gp[p]) * ivar[p]
         self.phi_sum += phi
         self.A_sum += A
-        assert np.allclose(self.A_sum, self.A_sum.T)
-        '''
+
+        return gp, phi, A
+
+    def psf_convolve(self, lo, hi, psf, out):
+        """Convolve psf with a pixel spanning [lo,hi] and save results in out.
+        """
+        # Find the closest grid indices to lo, hi.
+        edges = np.array([lo, hi]) - 0.5 * self.grid_scale
+        idx_lo, idx_hi = np.searchsorted(self.grid, edges)
+        assert np.abs(lo - self.grid[idx_lo]) <= 0.5 * self.grid_scale
+        assert np.abs(hi - self.grid[idx_hi]) <= 0.5 * self.grid_scale
+        pixel = np.ones(idx_hi - idx_lo + 1, dtype=out.dtype)
+        n = (self.n_psf - 1) // 2
+        assert idx_lo >= n and idx_hi < self.n_grid - n
+        out[idx_lo - n: idx_hi + n + 1] = np.convolve(pixel, psf, mode='full')
+
+    def psf_center(self, lo, hi, psf, out):
+        """Center psf in a pixel spanning [lo,hi] and save results in out.
+        """
+        wmid = 0.5 * (lo + hi)
+        # Find the closest grid index to wmid.
+        idx = np.searchsorted(self.grid, [wmid - 0.5 * self.grid_scale])[0]
+        assert np.abs(wmid - self.grid[idx]) <= 0.5 * self.grid_scale
+        n = (self.n_psf - 1) // 2
+        out[idx - n: idx + n + 1] = psf
--- coadd/tests/test_spectra.py
+++ coadd/tests/test_spectra.py
@@ -4,4 +4,45 @@ from coadd.spectra import *
 
 
 def test_ctor():
-    pass
+    """Normal construction.
+    """
+    c = CoAdder(100., 200., 1., 10.)
+    assert c.grid[0] == 100.
+    assert c.grid[-1] == 200.
+    assert c.grid_scale == 1.
+    assert np.all(c.psf_grid == np.arange(-10., 11.))
+
+
+def test_add_psf():
+    """Addition with different types of PSF inputs.
+    """
+    c = CoAdder(100., 200., 0.5, 10.)
+    psf = np.zeros(c.n_psf)
+    psf[c.n_psf // 2] = 1
+    psfs = np.tile(psf, [3, 1])
+    args = [1, 3, 2], [150, 160, 170, 180], [0.1, 0.2, 0.1]
+    for convolve in True, False:
+        c.add(*args, 5, convolve)
+        c.add(*args, [4, 5, 6], convolve)
+        c.add(*args, psf, convolve)
+        c.add(*args, psfs, convolve)
+
+
+def test_add_analytic_vs_tabulated():
+    """Compare analytic vs tabulated Gaussian PSFs.
+    """
+    c = CoAdder(100., 200., 0.5, 10.)
+    # Common PSF for all pixels.
+    rms = 1.5
+    gp0 = c.add([1, 3, 2], [150, 160, 170, 180], [0.1, 0.2, 0.1], rms)
+    psf = np.exp(-0.5 * (c.psf_grid / rms) ** 2)
+    psf /= psf.sum()
+    gp1 = c.add([1, 3, 2], [150, 160, 170, 180], [0.1, 0.2, 0.1], psf)
+    assert np.allclose(gp0, gp1, atol=0.05, rtol=0.05)
+    # Individual PSFs for each pixel.
+    rms = np.array([1.4, 1.5, 1.6])
+    gp0 = c.add([1, 3, 2], [150, 160, 170, 180], [0.1, 0.2, 0.1], rms)
+    psf = np.exp(-0.5 * (c.psf_grid / rms.reshape(-1, 1)) ** 2)
+    psf /= psf.sum(axis=1).reshape(-1, 1)
+    gp1 = c.add([1, 3, 2], [150, 160, 170, 180], [0.1, 0.2, 0.1], psf)
+    assert np.allclose(gp0, gp1, atol=0.05, rtol=0.05)
